761b73f7a9a55e9b660d13293e445fb4adb1ac9f
CastelWebs <152329148+CastelWebs@users.noreply.github.com>
2024-10-15 18:29:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -217,3 +217,16 @@ document.getElementById('registroForm').addEventListener('submit', function(e) {
     });
 });
 
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./serviceWorker.js')
+      .then((registration) => {
+        console.log('Service Worker registrado con éxito:', registration);
+      })
+      .catch((error) => {
+        console.log('Error al registrar el Service Worker:', error);
+      });
+  });
+}
+
